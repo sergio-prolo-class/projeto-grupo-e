2651aa02b74779cfa3e3459fe19fac408b33af92
raul-s-teodoro <raul.ts@aluno.ifsc.edu.br>
2025-12-11 23:18:12 +0000

    Adiciona tentativa de validar as cores nas posições

--- projetoR/decodificadorR.java
+++ projetoR/decodificadorR.java
@@ -12,11 +12,21 @@ public class decodificadorR{
         //o array cores1 recebe os valores digitados em cores já separados, trampo do "cores.split(" ")" que dividiu por espaços na entrada do usuario 
         String[] cores1 = cores.split(" ");
         
+
+        //tentativa de validador de cores em posições inválidas(nao deu certo ainda)
+        if(cores1.length == 4){
+            if(cores1[4] == "laranja" || cores1[4] == "marrom"){
+                System.out.printf("A cor da faixa (%s), não é válida para sua posição %n", cores1[4]);
+            }
+        } 
+
         while(sc.hasNextLine()){
         //lista todas as cores e suas posições no array cores1
         for(int i = 0; i <= cores1.length; i++){
             System.out.println("Cor " + i + ": " + cores1[i]);
+            }
         }
-    }
+
+        System.out.println(cores1.length);
     }
 }
