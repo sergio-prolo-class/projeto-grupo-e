aab17f83022d43528879087cb47bbd8f07dfce8a
raul-s-teodoro <raul.ts@aluno.ifsc.edu.br>
2025-12-13 18:20:28 +0000

    Entendendo melhor a quebra de linha da estrutura de repetição

--- projetoR/decodificadorR.java
+++ projetoR/decodificadorR.java
@@ -7,26 +7,45 @@ public class decodificadorR{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        /* 
+
+
         // cores recebe a linha inteira
         String cores = sc.nextLine();
         //o array cores1 recebe os valores digitados em cores já separados, trampo do "cores.split(" ")" que dividiu por espaços na entrada do usuario 
         String[] cores1 = cores.split(" ");
-        
-
-        //tentativa de validador de cores em posições inválidas(nao deu certo ainda)
         if(cores1.length == 4){
-            if(cores1[4] == "laranja" || cores1[4] == "marrom"){
-                System.out.printf("A cor da faixa (%s), não é válida para sua posição %n", cores1[4]);
-            }
-        } 
-
-        while(sc.hasNextLine()){
-        //lista todas as cores e suas posições no array cores1
-        for(int i = 0; i <= cores1.length; i++){
-            System.out.println("Cor " + i + ": " + cores1[i]);
+            if(cores1[3] == "laranja "){
+        System.out.printf("%s %n", cores1);
             }
         }
+        
+        if(cores1.length == 4){
+            if(cores1[3] == "laranja" || cores1[3] == "marrom"){
+                System.out.printf("A cor da faixa (%s), não é válida para sua posição %n", cores1[3]);
+            }
+        } 
+            
 
-        System.out.println(cores1.length);
+        */
+
+        
+        while(sc.hasNextLine()){
+            // le a linha inteira
+            String cores = sc.nextLine();
+            // divide as cores por espaço e bota no array cores1
+            String[] cores1 = cores.split(" ");
+
+
+        
+        for(int i = 0; i < cores1.length; i++){
+            //numera e lista as cores da linha atual
+            System.out.printf("faixa %d: %s ", i, cores1[i]);
+            }
+            //quebra a linha, e se tiver mais uma linha o programa começa de novo
+            System.out.println();
+        }
+
+        
     }
 }
