6872ed3a26555e8102f0f64940ceeb9de751c4ba
AugSZ <vitoraugusto.szslv@gmail.com>
2025-12-14 15:09:50 -0300

    adicionado loop para encontrar e multiplicar valores dos digitos, reorganização do código e adicionado coefTemp

--- DecodificadorResistor.java
+++ DecodificadorResistor.java
@@ -36,8 +36,8 @@ public class DecodificadorResistor{
             //define tamanho do resistor
             int tamanhoResistor = 0;
             for (String cor: coresResistor) {
-            if (cor != null || cor.isEmpty())
-                tamanhoResistor++;
+                if (cor != null && !cor.isEmpty())
+                    tamanhoResistor++;
            } 
            
            //Se o resistor for menor que 4, é invalido
@@ -47,18 +47,77 @@ public class DecodificadorResistor{
                 return;
            }
 
-           if (tamanhoResistor==4) {
-                double digito1 = encontrarValorDigitos(coresResistor[0]); // chama a função que converte nome da cor em numero inteiro
-                double digito2 = encontrarValorDigitos(coresResistor[1]); // chama a função que converte nome da cor em numero inteiro
-                double multiplicador = Math.pow(10, (encontrarValorDigitos(coresResistor[2])));
-                double tolerancia = calculoTolerancia(coresResistor[3]); // chama função específica para tolerância
+           if (tamanhoResistor >= 4 && tamanhoResistor <= 6) {
+               int numDigits = (tamanhoResistor == 4) ? 2 : 3; //resistor tem 4 cores? Se sim ele tem 2 digitos. Senão, todos os outros em 3 digitos
 
-                //Dá erro se o valor de digito1 ou digito2 for inválido
-                if (digito1 < 0 || Double.isNaN(digito1) || digito2 < 0 || Double.isNaN(digito2)){
-                    System.out.println("Nome de cor inválido. Tente preto, marrom, vermelho, laranja, amarelo, verde, azul, violeta, cinza ou branco");
-                    sc.close();
-                    return;
-                }
+               double[] digitos = new double[numDigits]; // array do tamanho da quantidade de digitos, 2 ou 3.
+               boolean invalido = false;
+
+               for(int i = 0; i < numDigits; i++){ // loop que converte as cores em valores
+                   digitos[i] = encontrarValorDigitos(coresResistor[i]);
+
+                   if(digitos[i] < 0 || Double.isNaN(digitos[i])){
+                       invalido = true; // valida se o valor é valido
+                   }
+               }
+
+
+               if(invalido){ //se for invalido
+                   System.out.println("Nome de cor inválido para dígitos. Tente preto, marrom, vermelho, laranja, amarelo, verde, azul, violeta, cinza ou branco");
+                   sc.close();
+                   return; //acaba
+               }
+
+               double multiplicador = encontrarValorDigitos(coresResistor[numDigits]);
+               if(Double.isNaN(multiplicador)){
+                   System.out.println("Nome de cor inválido para multiplicador"); //ajuda a especificar melhor a cor que deu erro
+                   sc.close();
+                   return;
+               }
+
+               // acessa calculoTolerancia para já converter cor em valor
+               double tolerancia = calculoTolerancia(coresResistor[numDigits + 1]); // acessa a cor do multiplicador, independente do tamanho do resistor
+               if(Double.isNaN(tolerancia)){//se for valor vazio
+                   System.out.println("Nome de cor inválido para tolerância");
+                   sc.close();
+                   return; 
+               }
+
+                /*Sobre o for a seguir:
+                funciona se tem 2 ou 3 digitos, pois tem como base o número de digitos.
+                pega o primeiro valor, multiplicado por 10 elevado a quantidade - 1 - o iterador
+                Digamos que os digitos sejam 1, 2 e 3. 
+                Se tivermos dois digitos, 1*10^(2-1-0) -> 1*10^1 -> 10
+                Em seguida o segundo digito, 2*10^(2-1-1) -> 2*10^0 = 2*1 = o próprio 2
+        
+                Se tiver tres digitos, seria 1*10^(3-1-0) (a quantidade de digitos, faz o que seria 10 virar 100)
+                depois 2*10^(3-1-1), mesma coisa, o que era 2 vira 20
+                E aqui, o 3 fica três mesmo
+                */
+
+
+               double valor = 0;
+               for(int i = 0; i < numDigits; i++){
+                   valor += digitos[i] * Math.pow(10, numDigits - 1 - i);
+               }
+
+               //O valor do multiplicador não passa do expoente, de uma notação científica
+               valor *= Math.pow(10, multiplicador);
+               System.out.printf("Seu resistor tem: %.2f Ohms (+- %.2f%%)\n", valor, tolerancia);
+
+               //Se o resistor tem 6 cores, ele tem um coeficiente de temperatura
+               if(tamanhoResistor == 6){
+                   double coefTemp = coeficienteDeTemperatura(coresResistor[5]);
+                   if(!Double.isNaN(coefTemp)){
+                       System.out.printf("Coeficiente de temperatura: %.0f ppm/K\n", coefTemp);
+                   } else {
+                       System.out.println("Nome de cor inválido para coeficiente de temperatura");
+                   }
+               }
+           } else { //Se não tiver entre 4 e 6
+               System.out.println("Tamanho de resistor inválido. Este deve conter entre 4 e 6 cores");
+               sc.close();
+               return;
            }
         }
         
@@ -69,8 +128,12 @@ public class DecodificadorResistor{
 
     static double encontrarValorDigitos (String cor) {
         switch (cor.trim().toUpperCase()) { // funciona escrevendo vermelho ou VERMELHO
-            case "PRATEADO" : return -2;
-            case "DOURADO" : return -1; 
+            case "PRATEADO":
+            case "PRATA":
+                return -2;
+            case "DOURADO":
+            case "OURO" : 
+                return -1; 
             case "PRETO" : return 0; 
             case "MARROM" : return 1;
             case "VERMELHO" : return 2;
@@ -88,8 +151,12 @@ public class DecodificadorResistor{
 
     static double calculoTolerancia (String cor) {
         switch (cor.trim().toUpperCase()) { // funciona escrevendo vermelho ou VERMELHO
-            case "PRATEADO" : return 10;
-            case "DOURADO" : return 5;  
+            case "PRATEADO" : 
+            case "PRATA": 
+                return 10;
+            case "DOURADO" : 
+            case "OURO":
+                return 5;  
             case "MARROM" : return 1;
             case "VERMELHO" : return 2;
             case "VERDE" : return 0.5;
