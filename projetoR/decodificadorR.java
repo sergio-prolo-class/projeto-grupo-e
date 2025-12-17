b34ac6406fffb89977bce34a9a9b8d9f89f23eaf
raul-s-teodoro <raul.ts@aluno.ifsc.edu.br>
2025-12-17 00:37:14 +0000

    Limpa os if else(só pra ver como ficaria)

--- projetoR/decodificadorR.java
+++ projetoR/decodificadorR.java
@@ -33,11 +33,11 @@ public class decodificadorR{
                 valoresSeisFaixas(cores1);
                 System.out.printf("%.2f ohms, %s de tolerancia e %s de coeficiente de temperatura", (digitosT5 * faixa4), faixa5, faixa6);
             } else if (faixa1 == 99 || faixa2 == 99 || faixa3 == 99 || faixa4 == 99 || faixa5.equals("Cor Inválida para a posição") || faixa6.equals("Cor Inválida para a posição")){
-                System.out.print("Cor Inválida");
+                System.out.print("Cor Inválida para a posição");
             } else {
                 System.out.print("Número inválido de faixas");
             }
-
+            if (cores.trim().isEmpty()) continue;
             System.out.println();
         }
 
@@ -45,345 +45,100 @@ public class decodificadorR{
 
     }
 
-        static void valoresQuatroFaixas(String cores1[]){
+    static int getValorCor(String cor) {
+        return switch(cor) {
+            case "preto" -> 0;
+            case "marrom" -> 1;
+            case "vermelho" -> 2;
+            case "laranja" -> 3;
+            case "amarelo" -> 4;
+            case "verde" -> 5;
+            case "azul" -> 6;
+            case "roxo" -> 7;
+            case "cinza" -> 8;
+            case "branco" -> 9;
+            default -> 99;
+        };
+    }
 
-                if(cores1[0].equals("preto")){
-                    faixa1 = 0; 
-                } else if(cores1[0].equals("marrom")){
-                    faixa1 = 1;
-                } else if(cores1[0].equals("vermelho")){
-                    faixa1 = 2;
-                } else if(cores1[0].equals("laranja")){
-                    faixa1 = 3;
-                } else if(cores1[0].equals("amarelo")){
-                    faixa1 = 4;
-                } else if(cores1[0].equals("verde")){
-                    faixa1 = 5;
-                } else if(cores1[0].equals("azul")){
-                    faixa1 = 6;
-                } else if(cores1[0].equals("roxo")){
-                    faixa1 = 7;
-                } else if(cores1[0].equals("cinza")){
-                    faixa1 = 8;
-                } else if(cores1[0].equals("branco")){
-                    faixa1 = 9;
-                } else
-                    faixa1 = 99;
+    static double getValorMultiplicador(String cor) {
+        return switch(cor) {
+            case "prata" -> 0.01;
+            case "ouro" -> 0.1;
+            case "preto" -> 1;
+            case "marrom" -> 10;
+            case "vermelho" -> 100;
+            case "laranja" -> 1000;
+            case "amarelo" -> 10000;
+            case "verde" -> 100000;
+            case "azul" -> 1000000;
+            case "roxo" -> 10000000;
+            default -> 99;
+        };
+    }
 
-                if(cores1[1].equals("preto")){
-                    faixa2 = 0; 
-                } else if(cores1[1].equals("marrom")){
-                    faixa2 = 1;
-                } else if(cores1[1].equals("vermelho")){
-                    faixa2 = 2;
-                } else if(cores1[1].equals("laranja")){
-                    faixa2 = 3;
-                } else if(cores1[1].equals("amarelo")){
-                    faixa2 = 4;
-                } else if(cores1[1].equals("verde")){
-                    faixa2 = 5;
-                } else if(cores1[1].equals("azul")){
-                    faixa2 = 6;
-                } else if(cores1[1].equals("roxo")){
-                    faixa2 = 7;
-                } else if(cores1[1].equals("cinza")){
-                    faixa2 = 8;
-                } else if(cores1[1].equals("branco")){
-                    faixa2 = 9;
-                } else 
-                    faixa2 = 99;
+    static String getValorTolerancia4Faixas(String cor) {
+        return switch(cor) {
+            case "prata" -> "10%";
+            case "amarelo" -> "5%";
+            case "marrom" -> "1%";
+            case "vermelho" -> "2%";
+            case "verde" -> "0.5%";
+            case "azul" -> "0.25%";
+            case "roxo" -> "0.1%";
+            case "cinza" -> "0.05%";
+            default -> "Cor Inválida para a posição";
+        };
+    }
 
-                if(cores1[2].equals("prata")){
-                    faixa4 = 0.01; 
-                } else if(cores1[2].equals("ouro")){
-                    faixa4 = 0.1;
-                } else if(cores1[2].equals("preto")){
-                    faixa4 = 1;
-                } else if(cores1[2].equals("marrom")){
-                    faixa4 = 10;
-                } else if(cores1[2].equals("vermelho")){
-                    faixa4 = 100;
-                } else if(cores1[2].equals("laranja")){
-                    faixa4 = 1000;
-                } else if(cores1[2].equals("amarelo")){
-                    faixa4 = 10000;
-                } else if(cores1[2].equals("verde")){
-                    faixa4 = 100000;
-                } else if(cores1[2].equals("azul")){
-                    faixa4 = 1000000;
-                } else if(cores1[2].equals("roxo")){
-                    faixa4 = 10000000;
-                } else
-                    faixa4 = 99;
+    static String getValorTolerancia5e6Faixas(String cor) {
+        return switch(cor) {
+            case "prata" -> "10%";
+            case "ouro" -> "5%";
+            case "marrom" -> "1%";
+            case "vermelho" -> "4%";
+            case "verde" -> "0.5%";
+            case "azul" -> "0.25%";
+            case "roxo" -> "0.1%";
+            case "cinza" -> "0.05%";
+            default -> "Cor Inválida para a posição";
+        };
+    }
 
-                if(cores1[3].equals("prata")){
-                    faixa5 = "10%"; 
-                } else if(cores1[3].equals("amarelo")){
-                    faixa5 = "5%";
-                } else if(cores1[3].equals("marrom")){
-                    faixa5 = "1%";
-                } else if(cores1[3].equals("vermelho")){
-                    faixa5 = "2%";
-                } else if(cores1[3].equals("verde")){
-                    faixa5 = "0.5%";
-                } else if(cores1[3].equals("azul")){
-                    faixa5 = "0.25%";
-                } else if(cores1[3].equals("roxo")){
-                    faixa5 = "0.1%";
-                } else if(cores1[3].equals("cinza")){
-                    faixa5 = "0.05%";
-                } else 
-                    faixa5 = "Cor Inválida para a posição";
+    static String getValorTemperatura(String cor) {
+        return switch(cor) {
+            case "marrom" -> "100ppm/K";
+            case "vermelho" -> "50ppm/K";
+            case "laranja" -> "15ppm/K";
+            case "amarelo" -> "25ppm/K";
+            default -> "Cor Inválida para a posição";
+        };
+    }
 
-                digitosT4 = (faixa1 * 10) + faixa2;
-            }
+    static void valoresQuatroFaixas(String cores1[]){
+        faixa1 = getValorCor(cores1[0]);
+        faixa2 = getValorCor(cores1[1]);
+        faixa4 = getValorMultiplicador(cores1[2]);
+        faixa5 = getValorTolerancia4Faixas(cores1[3]);
+        digitosT4 = (faixa1 * 10) + faixa2;
+    }
 
+    static void valoresCincoFaixas(String cores1[]){
+        faixa1 = getValorCor(cores1[0]);
+        faixa2 = getValorCor(cores1[1]);
+        faixa3 = getValorCor(cores1[2]);
+        faixa4 = getValorMultiplicador(cores1[3]);
+        faixa5 = getValorTolerancia5e6Faixas(cores1[4]);
+        digitosT5 = (faixa1 * 100) + (faixa2 * 10) + faixa3;
+    }
 
-
-            static void valoresCincoFaixas(String cores1[]){
-
-                if(cores1[0].equals("preto")){
-                    faixa1 = 0; 
-                } else if(cores1[0].equals("marrom")){
-                    faixa1 = 1;
-                } else if(cores1[0].equals("vermelho")){
-                    faixa1 = 2;
-                } else if(cores1[0].equals("laranja")){
-                    faixa1 = 3;
-                } else if(cores1[0].equals("amarelo")){
-                    faixa1 = 4;
-                } else if(cores1[0].equals("verde")){
-                    faixa1 = 5;
-                } else if(cores1[0].equals("azul")){
-                    faixa1 = 6;
-                } else if(cores1[0].equals("roxo")){
-                    faixa1 = 7;
-                } else if(cores1[0].equals("cinza")){
-                    faixa1 = 8;
-                } else if(cores1[0].equals("branco")){
-                    faixa1 = 9;
-                } else
-                    faixa1 = 99;
-
-                if(cores1[1].equals("preto")){
-                    faixa2 = 0; 
-                } else if(cores1[1].equals("marrom")){
-                    faixa2 = 1;
-                } else if(cores1[1].equals("vermelho")){
-                    faixa2 = 2;
-                } else if(cores1[1].equals("laranja")){
-                    faixa2 = 3;
-                } else if(cores1[1].equals("amarelo")){
-                    faixa2 = 4;
-                } else if(cores1[1].equals("verde")){
-                    faixa2 = 5;
-                } else if(cores1[1].equals("azul")){
-                    faixa2 = 6;
-                } else if(cores1[1].equals("roxo")){
-                    faixa2 = 7;
-                } else if(cores1[1].equals("cinza")){
-                    faixa2 = 8;
-                } else if(cores1[1].equals("branco")){
-                    faixa2 = 9;
-                } else 
-                    faixa2 = 99;
-
-                if(cores1[2].equals("preto")){
-                    faixa3 = 0; 
-                } else if(cores1[2].equals("marrom")){
-                    faixa3 = 1;
-                } else if(cores1[2].equals("vermelho")){
-                    faixa3 = 2;
-                } else if(cores1[2].equals("laranja")){
-                    faixa3 = 3;
-                } else if(cores1[2].equals("amarelo")){
-                    faixa3 = 4;
-                } else if(cores1[2].equals("verde")){
-                    faixa3 = 5;
-                } else if(cores1[2].equals("azul")){
-                    faixa3 = 6;
-                } else if(cores1[2].equals("roxo")){
-                    faixa3 = 7;
-                } else if(cores1[2].equals("cinza")){
-                    faixa3 = 8;
-                } else if(cores1[2].equals("branco")){
-                    faixa3 = 9;
-                } else
-                    faixa3 = 99;
-
-                if(cores1[3].equals("prata")){
-                    faixa4 = 0.01; 
-                } else if(cores1[3].equals("ouro")){
-                    faixa4 = 0.1;
-                } else if(cores1[3].equals("preto")){
-                    faixa4 = 1;
-                } else if(cores1[3].equals("marrom")){
-                    faixa4 = 10;
-                } else if(cores1[3].equals("vermelho")){
-                    faixa4 = 100;
-                } else if(cores1[3].equals("laranja")){
-                    faixa4 = 1000;
-                } else if(cores1[3].equals("amarelo")){
-                    faixa4 = 10000;
-                } else if(cores1[3].equals("verde")){
-                    faixa4 = 100000;
-                } else if(cores1[3].equals("azul")){
-                    faixa4 = 1000000;
-                } else if(cores1[3].equals("roxo")){
-                    faixa4 = 10000000;
-                } else
-                    faixa4 = 99;
-
-                if(cores1[4].equals("prata")){
-                    faixa5 = "10%"; 
-                } else if(cores1[4].equals("ouro")){
-                    faixa5 = "5%";
-                } else if(cores1[4].equals("marrom")){
-                    faixa5 = "1%";
-                } else if(cores1[4].equals("vermelho")){
-                    faixa5 = "4%";
-                } else if(cores1[4].equals("verde")){
-                    faixa5 = "0.5%";
-                } else if(cores1[4].equals("azul")){
-                    faixa5 = "0.25%";
-                } else if(cores1[4].equals("roxo")){
-                    faixa5 = "0.1%";
-                } else if(cores1[4].equals("cinza")){
-                    faixa5 = "0.05%";
-                } else 
-                    faixa5 = "Cor Inválida para a posição";
-
-                digitosT5 = (faixa1 * 100) + (faixa2 * 10) + faixa3;
-            }
-
-
-            static void valoresSeisFaixas(String cores1[]){
-
-                if(cores1[0].equals("preto")){
-                    faixa1 = 0; 
-                } else if(cores1[0].equals("marrom")){
-                    faixa1 = 1;
-                } else if(cores1[0].equals("vermelho")){
-                    faixa1 = 2;
-                } else if(cores1[0].equals("laranja")){
-                    faixa1 = 3;
-                } else if(cores1[0].equals("amarelo")){
-                    faixa1 = 4;
-                } else if(cores1[0].equals("verde")){
-                    faixa1 = 5;
-                } else if(cores1[0].equals("azul")){
-                    faixa1 = 6;
-                } else if(cores1[0].equals("roxo")){
-                    faixa1 = 7;
-                } else if(cores1[0].equals("cinza")){
-                    faixa1 = 8;
-                } else if(cores1[0].equals("branco")){
-                    faixa1 = 9;
-                } else
-                    faixa1 = 99;  
-                
-
-                if(cores1[1].equals("preto")){
-                    faixa2 = 0; 
-                } else if(cores1[1].equals("marrom")){
-                    faixa2 = 1;
-                } else if(cores1[1].equals("vermelho")){
-                    faixa2 = 2;
-                } else if(cores1[1].equals("laranja")){
-                    faixa2 = 3;
-                } else if(cores1[1].equals("amarelo")){
-                    faixa2 = 4;
-                } else if(cores1[1].equals("verde")){
-                    faixa2 = 5;
-                } else if(cores1[1].equals("azul")){
-                    faixa2 = 6;
-                } else if(cores1[1].equals("roxo")){
-                    faixa2 = 7;
-                } else if(cores1[1].equals("cinza")){
-                    faixa2 = 8;
-                } else if(cores1[1].equals("branco")){
-                    faixa2 = 9;
-                } else 
-                    faixa2 = 99;
-
-                if(cores1[2].equals("preto")){
-                    faixa3 = 0; 
-                } else if(cores1[2].equals("marrom")){
-                    faixa3 = 1;
-                } else if(cores1[2].equals("vermelho")){
-                    faixa3 = 2;
-                } else if(cores1[2].equals("laranja")){
-                    faixa3 = 3;
-                } else if(cores1[2].equals("amarelo")){
-                    faixa3 = 4;
-                } else if(cores1[2].equals("verde")){
-                    faixa3 = 5;
-                } else if(cores1[2].equals("azul")){
-                    faixa3 = 6;
-                } else if(cores1[2].equals("roxo")){
-                    faixa3 = 7;
-                } else if(cores1[2].equals("cinza")){
-                    faixa3 = 8;
-                } else if(cores1[2].equals("branco")){
-                    faixa3 = 9;
-                } else
-                    faixa3 = 99;
-
-                if(cores1[3].equals("prata")){
-                    faixa4 = 0.01; 
-                } else if(cores1[3].equals("ouro")){
-                    faixa4 = 0.1;
-                } else if(cores1[3].equals("preto")){
-                    faixa4 = 1;
-                } else if(cores1[3].equals("marrom")){
-                    faixa4 = 10;
-                } else if(cores1[3].equals("vermelho")){
-                    faixa4 = 100;
-                } else if(cores1[3].equals("laranja")){
-                    faixa4 = 1000;
-                } else if(cores1[3].equals("amarelo")){
-                    faixa4 = 10000;
-                } else if(cores1[3].equals("verde")){
-                    faixa4 = 100000;
-                } else if(cores1[3].equals("azul")){
-                    faixa4 = 1000000;
-                } else if(cores1[3].equals("roxo")){
-                    faixa4 = 10000000;
-                } else
-                    faixa4 = 99;
-
-                if(cores1[4].equals("prata")){
-                    faixa5 = "10%"; 
-                } else if(cores1[4].equals("ouro")){
-                    faixa5 = "5%";
-                } else if(cores1[4].equals("marrom")){
-                    faixa5 = "1%";
-                } else if(cores1[4].equals("vermelho")){
-                    faixa5 = "4%";
-                } else if(cores1[4].equals("verde")){
-                    faixa5 = "0.5%";
-                } else if(cores1[4].equals("azul")){
-                    faixa5 = "0.25%";
-                } else if(cores1[4].equals("roxo")){
-                    faixa5 = "0.1%";
-                } else if(cores1[4].equals("cinza")){
-                    faixa5 = "0.05%";
-                } else 
-                    faixa5 = "Cor Inválida para a posição";
-
-                if(cores1[5].equals("marrom")){
-                    faixa6 = "100ppm/K"; 
-                } else if(cores1[5].equals("vermelho")){
-                    faixa6 = "50ppm/K";
-                } else if(cores1[5].equals("laranja")){
-                    faixa6 = "15ppm/K";
-                } else if(cores1[5].equals("amarelo")){
-                    faixa6 = "25ppm/K";
-                } else 
-                    faixa6 = "Cor Inválida para a posição";
-
-                digitosT5 = (faixa1 * 100) + (faixa2 * 10) + faixa3;
-            }
-
-            
-        }
+    static void valoresSeisFaixas(String cores1[]){
+        faixa1 = getValorCor(cores1[0]);
+        faixa2 = getValorCor(cores1[1]);
+        faixa3 = getValorCor(cores1[2]);
+        faixa4 = getValorMultiplicador(cores1[3]);
+        faixa5 = getValorTolerancia5e6Faixas(cores1[4]);
+        faixa6 = getValorTemperatura(cores1[5]);
+        digitosT5 = (faixa1 * 100) + (faixa2 * 10) + faixa3;
+    }
+}
